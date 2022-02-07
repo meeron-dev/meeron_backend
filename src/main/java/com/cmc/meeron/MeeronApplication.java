87a31e3ae11068e18a4d3d2746bb32b1f079b27f
kobeomseok95 <kobumssh@naver.com>
2022-02-07 18:57:15 +0900

    initial commit

---
+++ src/main/java/com/cmc/meeron/MeeronApplication.java
@@ -0,0 +1,13 @@
+package com.cmc.meeron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeeronApplication.class, args);
+    }
+
+}
