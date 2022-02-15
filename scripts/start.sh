cce5f8861595196cb345165dfed91f9659f94d1c
kobeomseok95 <kobumssh@naver.com>
2022-02-15 14:34:16 +0900

    실행권한 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,9 @@
 echo "[dev-meeron] 작업 디렉토리 위치 변경"
 cd /home/ec2-user/app
 
+echo "[dev-meeron] 실행 권한 추가"
+chmod +x start.sh
+
 echo "[dev-meeron] 새 배포 dev 환경 애플리케이션 build"
 echo "[dev-meeron] jasypt password = $JASYPT_MEERON_PASSWORD"
 docker build --build-arg JASPYT_PASSWORD=$JASYPT_MEERON_PASSWORD -t dev-meeron:latest .
