fc751cb9723e4a851f505ef73d94b2279b4afb1b
kobeomseok95 <kobumssh@naver.com>
2022-02-15 14:41:36 +0900

    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,8 @@
 echo "[dev-meeron] 작업 디렉토리 위치 변경"
 cd /home/ec2-user/app
 
-echo "[dev-meeron] 실행 권한 추가"
-chmod +x start.sh
+echo "[dev-meeron] JASPYT_PASSWORD 변수 설정"
+JASYPT_MEERON_PASSWORD=$JASYPT_MEERON_PASSWORD
 
 echo "[dev-meeron] 새 배포 dev 환경 애플리케이션 build"
 echo "[dev-meeron] jasypt password = $JASYPT_MEERON_PASSWORD"
