502f416eb667eed530f8603790c0cd45b3680afe
kobeomseok95 <kobumssh@naver.com>
2022-03-23 23:24:07 +0900

    mysql, redis 설정, github action 분기문 추가

---
+++ src/main/java/com/cmc/meeron/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.cmc.meeron.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Profile("prod")
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+}
